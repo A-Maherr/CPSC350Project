7c019c0ad6e187623ed0c30d17eb27b0cd4c6e4f
Ahmed <ahmed5551111@yahoo.com>
2025-11-02 00:08:18 -0400

    Log in Page works now

--- LogIn.py
+++ LogIn.py
@@ -1,9 +1,24 @@
 import streamlit as st
+import pandas as pd
+import time 
+conn = st.connection('dataset_db', type='sql')
 
 def login_page():
     st.title("Log In")
-
     username = st.text_input("Username", placeholder="Enter your username")
+
+    if(st.button("Log In", use_container_width=True, key = "login_button")):
+        if(username):
+            if not conn.query(f"SELECT * from users where username = '{username}';", ttl=0).empty:
+                st.success(f"Welcome back, {username}!")
+                time.sleep(2)
+                st.session_state.username = username
+                st.session_state.page = "main_app"
+                st.rerun()
+            else:
+                st.error("Username not found. Please create an account.")
+        else:
+            st.error("Please enter your username to log in.")
 
     col1, col2 = st.columns([1, 1])
     with col1:
